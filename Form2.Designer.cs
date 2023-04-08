0221272baaed6c3ccef386ca2b4c86366600d006
phanan11 <127832890+phanan11@users.noreply.github.com>
2023-04-08 22:06:39 +0700

    qlsv

---
+++ Form2.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.treeView1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 425);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Majors";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Class";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 79);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Majors";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 79);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Add Class";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(202, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 79);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Remove Select Button";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(11, 221);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(280, 201);
+            this.treeView1.TabIndex = 7;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(434, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Student Management";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(330, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Student Code";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(330, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(330, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Birthday";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(438, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(323, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(438, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(323, 26);
+            this.textBox4.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(438, 168);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 87);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Add";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(561, 168);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 87);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Fix";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(686, 168);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 87);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Delete";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(313, 261);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(475, 177);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(438, 136);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(323, 26);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Student Code";
+            this.columnHeader1.Width = 148;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 113;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Birthday";
+            this.columnHeader3.Width = 124;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Class";
+            this.columnHeader4.Width = 91;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form2 : Form
+    {
+        List<ListViewItem> list;
+        public Form2()
+        {
+            InitializeComponent();
+            list = new List<ListViewItem>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TreeNode noo = new TreeNode();
+            noo.Text = textBox1.Text;
+            treeView1.Nodes.Add(noo);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TreeNode noo = new TreeNode();
+            noo.Text = textBox2.Text;
+            treeView1.SelectedNode.Nodes.Add(noo);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult delete = MessageBox.Show("Are You Sure? ", "Notification",
+               MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            if (delete == DialogResult.Yes)
+            {
+                treeView1.SelectedNode.Remove();
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.Items.Add(textBox3.Text);
+            item.SubItems.Add(textBox4.Text);
+            item.SubItems.Add(dateTimePicker1.Value.ToShortDateString());
+            item.SubItems.Add(treeView1.SelectedNode.Text);
+
+            list.Add(item);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listView1.SelectedItems[0].SubItems[0].Text = textBox3.Text;
+            listView1.SelectedItems[0].SubItems[1].Text = textBox4.Text;
+            listView1.SelectedItems[0].SubItems[2].Text = dateTimePicker1.Value.ToShortDateString();
+            listView1.SelectedItems[0].SubItems[3].Text = treeView1.SelectedNode.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                listView1.Items.Remove(listView1.SelectedItems[0]);
+
+            }
+            else
+            {
+                MessageBox.Show("Select 1 object");
+
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string a = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (a == list[i].SubItems[3].Text)
+                {
+                    listView1.Items.Clear();
+                    ListViewItem item = listView1.Items.Add(list[i].SubItems[0].Text);
+                    item.SubItems.Add(list[i].SubItems[1].Text);
+                    item.SubItems.Add(list[i].SubItems[2].Text);
+                    item.SubItems.Add(list[i].SubItems[3].Text);
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count > 0)
+            {
+                textBox3.Text = listView1.SelectedItems[0].SubItems[0].Text;
+                textBox4.Text = listView1.SelectedItems[0].SubItems[0].Text;
+                dateTimePicker1.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            }
+        }
+    }
+}
